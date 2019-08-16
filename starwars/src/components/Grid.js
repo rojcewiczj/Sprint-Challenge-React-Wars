531b86e8b5c4682b405a15eaa07401942b5eda5c
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-16 11:05:48 -0500

    got a working axios call

--- starwars/src/components/Grid.js
+++ starwars/src/components/Grid.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
 import { Grid, Image } from 'semantic-ui-react'
 import CardExampleCard from "./Card"
 const GridExampleDividedNumber = () => {
-    
- const [characters, setCharacters] =useState([]);
 
+ const [characters, setCharacters] =useState([]);
+ useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/`, {
+        params: {}
+      })
+      .then(response => {
+          console.log(response.data.results);
+        
+      });
+  }, []);
 
 
 
