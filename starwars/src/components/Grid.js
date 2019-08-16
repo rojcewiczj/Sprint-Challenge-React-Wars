7fb4e7ae6e1196042e1dab1e0f0c3c1fd8c4dc0e
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-16 10:44:14 -0500

    implemented grid

---
+++ starwars/src/components/Grid.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Grid, Image } from 'semantic-ui-react'
+
+const GridExampleDividedNumber = () => (
+  <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+        
+      </Grid.Column>
+      <Grid.Column>
+       
+      </Grid.Column>
+      <Grid.Column>
+       
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        
+      </Grid.Column>
+      <Grid.Column>
+        
+      </Grid.Column>
+      <Grid.Column>
+       
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+export default GridExampleDividedNumber
